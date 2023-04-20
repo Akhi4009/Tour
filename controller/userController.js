c22bf174c987474f48e6dbdbd9ea374bd60aba24
Akhilesh yadav <akhileshyadav310596@gmail.com>
2023-04-20 16:41:08 +0530

    update user data handled

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,18 @@
 const User = require("../model/usermodel")
 const catchAsync = require("../utlits/catchAsync")
 const APIFeatures=require("../utlits/apiFeature")
+const AppError =require("../utlits/appError")
+
+const filteredObj=(obj,...allowedfields)=>{
+   
+    const newObj={}
+    Object.keys(obj).forEach(ele=>{
+        if(allowedfields.includes(ele)){
+            newObj[ele]=obj[ele]
+        }
+    })
+    return newObj
+}
 
 const getUser= catchAsync (async(req,res,next)=>{
     const features=new APIFeatures(User.find(),req.query).filter().sort().LimitFields().Pagination()
@@ -9,4 +21,27 @@ const getUser= catchAsync (async(req,res,next)=>{
         res.status(200).send({status:true,result:users.length,data:users})
   
         })
-  module.exports ={getUser}
+
+const updateMe=catchAsync(async(req,res,next)=>{
+
+    // create error if user posts password data
+    if(req.body.passwprd || req.body.passwordConfirm){
+        return next(AppError('This route is not for password updates. please use /updatepassword.',400))
+
+    }
+
+    // update user document
+    
+    const filteredBody=filteredObj(req.body,'name','email')
+    
+    const updatedUser=await User.findByIdAndUpdate(req.user.id,filteredBody,{
+        new:true,
+        runValidators:true
+    })
+    res.status(200).json({
+        status:"success",
+        user:updatedUser
+
+    })
+})
+  module.exports ={getUser,updateMe}
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,15 +3,18 @@ const router=express.Router()
 const User=require("../model/usermodel")
 
 const {signup,logIn,protect,restrictTo,forgetPassword,resetPassword, updatePassword} = require("../controller/authController")
-const {getUser} = require("../controller/userController")
+const {getUser,updateMe} = require("../controller/userController")
 
 router.post("/signup",signup)
 router.post("/login",logIn)
 router.post("/forgetpassword",forgetPassword)
 router.patch("/resetpassword/:token",resetPassword)
 router.patch("/updatepassword",protect, updatePassword)
+router.patch("/updateme",protect, updateMe)
 
 
 router.route("/")
 .get(protect,getUser)
+
+
 module.exports=router
