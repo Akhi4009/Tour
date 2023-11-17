eb464cd0bc1b606e964a5b17ff15b04d4c413c0f
Akhilesh yadav <akhileshyadav310596@gmail.com>
2023-11-17 16:28:36 +0530

    improvement

--- model/tourmodel.js
+++ model/tourmodel.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose")
 
 const tourSchema=mongoose.Schema({
-    "name":{type:String,required:true},
+    "name":{type:String,required:true,trim:true},
     "price":{type:Number,required:true},
     "duration":{type:Number,required:true},
    
